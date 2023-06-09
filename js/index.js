ab2a9c43158f7b67d19863d34bec81dcb896a862
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-09 08:01:12 -0700

    Add notes to each conditional to show what it is doing.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 function pigLatin(text) {
     const textArray = text.split(" ");
+        // textArray = ["hello", "world", "this", "is", "a", "test"]
     const vowels = ["a", "e", "i", "o", "u"];
-    const elementsWVowels = [];
+    const wordsToPL = [];
 
     textArray.forEach(function(element) {
         const char1 = element[0].toLowerCase();
         if (vowels.includes(char1)) {
-            elementsWVowels.push(element + "way")
+            wordsToPL.push(element + "way")
+            // wordsToPL = ["hello", "world", "this", "ISWAY", "AWAY", "test"]
         } else {
             const char2 = element[1].toLowerCase();
             if (vowels.includes(char2)) {
-                const element2 = element.slice(1);
-                elementsWVowels.push(element2 + char1 + "ay");
+                const elementWoFirstChar = element.slice(1);
+                wordsToPL.push(elementWoFirstChar + char1 + "ay");
+                // wordsToPL = ["ELLOHAY", "ORLDWAY", "this", "isway", "away", "ESTAY"]
             } else {
-                const element3 = element.slice(2);
-                elementsWVowels.push(element3 + char1 + char2 + "ay");
+                const elementWoSecondChar = element.slice(2);
+                wordsToPL.push(elementWoSecondChar + char1 + char2 + "ay");
+                // wordsToPL = ["ellohay", "orldway", "ISTHAY", "isway", "away", "esttay"]
             }
         }
     });
-    return elementsWVowels;
+    return wordsToPL;
 };
 
 const text = "hello world this is a test";
@@ -29,13 +33,13 @@ console.log(pigLatin(text));
 // function pigLatin(text) {
 //     const textArray = text.split(" ");
 //     const vowels = ["a", "e", "i", "o", "u"];
-//     const elementsWVowels = [];
+//     const wordsToPL = [];
 
 //     textArray.forEach(function(element) {
 //         const char1 = element[0].toLowerCase();
 //         if (vowels.includes(char1)) {
-//             elementsWVowels.push(element + "way")
+//             wordsToPL.push(element + "way")
 //         }
 //     })
-//     return elementsWVowels;
+//     return wordsToPL;
 // }
